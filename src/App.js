eff7d70c72ab78bf8e0c870cfe7670df13ca1b50
Atharv Agarwal <89630019+atharvagarwal@users.noreply.github.com>
2022-09-07 13:51:58 +0530

    Frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import RegisterForm from './Pages/RegisterForm'
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm/>
+    </div>
+  );
+}
+
+export default App;
